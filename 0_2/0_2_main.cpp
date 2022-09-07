09020a8c9aafc28ed043793c3a948db2b544a96f
rniman <shinmg00@gmail.com>
2022-09-07 14:56:41 +0900

    문장전체 뒤집기('d), 특수 문자 삽입/삭제('e') 완성

--- 0_2/0_2_main.cpp
+++ 0_2/0_2_main.cpp
@@ -68,7 +68,73 @@ int main()
 	cout <<"숫자의 개수: "<< number_num << endl;
 	cout << "대문자를 포함한 단어의 개수: " << capital_num << endl;
 
+	char command;
+	bool e_command = false;
+	ifs.seekg(0, ios::beg);
 
+	cin >> command;
+	while (command != 'q')
+	{
+		switch (command)
+		{
+		case 'd':
+			for (unsigned i = 0; i < str.size(); ++i)
+			{
+				string temp(str[i]);
+				for (unsigned j = 0; j < str[i].size(); ++j)
+				{
+					temp[j] = str[i][str[i].size() - j - 1];
+				}
+				str[i] = temp;
+			}
+			break;
+		case 'e':
+			if (!e_command)
+			{
+				for (unsigned i = 0; i < str.size(); ++i)
+				{
+					int insert_num = 0;
+					for (unsigned j = 0; j < str[i].size(); ++j)
+					{
+						if ((j - 2 * insert_num) % 3 == 2)
+						{
+							str[i].insert(j + 1, 2, '@');
+							insert_num++;
+							j += 2;
+						}
+					}
+				}
+			}
+			else
+			{
+				for (unsigned i = 0; i < str.size(); ++i)
+				{
+					str[i].erase(remove(str[i].begin(), str[i].end(), '@'), str[i].end());
+				}
+			}
+			e_command = e_command ? false : true;
+			break;
+
+		case 'f':
+			break;
+
+		case 'g':
+			break;
+
+		case 'h':
+			break;
+
+		default:
+			break;
+		}
+		cin.ignore();
+		cout << "\x1B[2J\x1B[H";
+		for (auto& e : str)
+		{
+			cout << e << endl;
+		}
+		cin >> command;
+	}
 	
 
 	return 0;
