a36c76615ea18146bdbc2253a63bc1bf7502bd2d
rniman <105342620+rniman@users.noreply.github.com>
2022-10-06 16:32:51 +0900

    실습11번 사각형 내부 판단 완료

--- 2_6_exercise/2_6_main.cpp
+++ 2_6_exercise/2_6_main.cpp
@@ -16,12 +16,15 @@ void initBuffer();
 
 GLvoid convert_OpenglXY_WindowXY(int& x, int& y, const float& ox, const float& oy);
 GLvoid convert_WindowXY_OpenglXY(const int& x, const int& y, float& ox, float& oy);
+GLint check_mouse_point(const GLfloat& ox, const GLfloat& oy, const std::vector<GLfloat>& ver);
+GLboolean isInside(const GLfloat& ox, const GLfloat& oy, const std::vector<GLfloat>& ver);
 
 
 const GLint window_w = 800, window_h = 600;
 GLfloat rColor = 0.5f, gColor = 0.5f, bColor = 0.5f;
 GLint oldx, oldy;
 GLint left_click = -1;
+
 
 GLuint vao;
 GLuint vbo[2];
@@ -40,7 +43,6 @@ std::vector<GLfloat> vertex =
 	0.5f, -0.5f, 0.0f,
 	0.5f, 0.5f, 0.0f*/
 };
-
 
 std::vector<GLfloat> color =
 {
@@ -70,6 +72,8 @@ int main(int argc, char** argv)
 
 	glutDisplayFunc(drawScene);
 	glutReshapeFunc(Reshape);
+	glutMouseFunc(MouseClick);
+	glutMotionFunc(MouseMotion);
 	glutKeyboardFunc(KeyEvent);
 
 	glutMainLoop();
@@ -85,10 +89,10 @@ GLvoid drawScene()
 
 	glBindVertexArray(vao);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-	
+
 	glDrawArrays(GL_LINE_LOOP, 0, vertex.size() / 3);
 
-	glPointSize(5);
+	glPointSize(10);
 	glDrawArrays(GL_POINTS, 0, vertex.size() / 3);
 
 	glutSwapBuffers();
@@ -101,14 +105,25 @@ GLvoid Reshape(int w, int h)
 
 GLvoid MouseClick(int button, int state, int x, int y)
 {
-	if (button == GLUT_LEFT && state == GLUT_DOWN)
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
 	{
 		//점이 어디를 눌렀는지
+		//점들
+		GLfloat temp_ox, temp_oy;
+		convert_WindowXY_OpenglXY(x, y, temp_ox, temp_oy);
 
+		left_click = check_mouse_point(temp_ox, temp_oy, vertex);
+		if (left_click >= 0)
+		{
+			oldx = x;
+			oldy = y;
+		}
+
+		//사각형 내부
 	}
-	else if (button == GLUT_LEFT && state == GLUT_UP)
+	else if (button == GLUT_LEFT_BUTTON && state == GLUT_UP)
 	{
-
+		left_click = -1;
 	}
 }
 
@@ -118,10 +133,16 @@ GLvoid MouseMotion(int x, int y)
 	{
 
 	}
-	else if (left_click == 1) //왼쪽 위점 클릭
+	else if (left_click > 0) //왼쪽 위점 클릭
 	{
-
+		GLfloat temp_ox, temp_oy;
+		convert_WindowXY_OpenglXY(x, y, temp_ox, temp_oy);
+		vertex[(left_click - 1) * 3] = temp_ox;
+		vertex[(left_click - 1) * 3 + 1] = temp_oy;
+		glBufferData(GL_ARRAY_BUFFER, vertex.size() * sizeof(GLfloat), vertex.data(), GL_STATIC_DRAW);
 	}
+
+	glutPostRedisplay();
 }
 
 GLvoid KeyEvent(unsigned char key, int x, int y)
@@ -134,7 +155,7 @@ void initBuffer()
 {
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
-	
+
 	glGenBuffers(2, vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
 	glBufferData(GL_ARRAY_BUFFER, color.size() * sizeof(GLfloat), color.data(), GL_STATIC_DRAW);
@@ -157,4 +178,41 @@ GLvoid convert_WindowXY_OpenglXY(const int& x, const int& y, float& ox, float& o
 {
 	ox = (float)((x - (float)window_w / 2.0) * (float)(1.0 / (float)(window_w / 2.0)));
 	oy = -(float)((y - (float)window_h / 2.0) * (float)(1.0 / (float)(window_h / 2.0)));
+}
+
+GLint check_mouse_point(const GLfloat& ox, const GLfloat& oy, const std::vector<GLfloat>& ver)
+{
+	for (int i = 0; i < 4; ++i)
+	{
+		if (ox > ver[i * 3] - 0.0125f && ox < ver[i * 3] + 0.0125f && oy > ver[i * 3 + 1] - 0.0125f && oy < ver[i * 3 + 1] + 0.0125f)
+		{
+			return i + 1;
+		}
+	}
+
+	if (isInside(ox, oy, ver))
+		return 0;
+
+	return -1;
+}
+
+GLboolean isInside(const GLfloat& ox, const GLfloat& oy, const std::vector<GLfloat>& ver)
+{
+	//crosses는 점q와 오른쪽 반직선과 다각형과의 교점의 개수 
+	int crosses = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		int j = (i + 1) % 4;
+		//점 B가 선분 (p[i], p[j])의 y좌표 사이에 있음    
+		if ((ver[i * 3 + 1] > oy) != (ver[j * 3 + 1] > oy))
+		{           
+			//atX는 점 B를 지나는 수평선과 선분 (p[i], p[j])의 교점     
+			double atX = (ver[j * 3] - ver[i * 3]) * (oy - ver[i * 3 + 1]) / (ver[j * 3 + 1] - ver[i * 3 + 1]) + ver[i * 3];
+			
+			//atX가 오른쪽 반직선과의 교점이 맞으면 교점의 개수를 증가시킨다.     
+			if (ox < atX)
+				crosses++;
+		}
+	}
+	return crosses % 2 > 0;
 }
