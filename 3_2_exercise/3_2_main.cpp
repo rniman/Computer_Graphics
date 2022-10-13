db5f37f2c38e8dcfadec009f6b57680360f61836
rniman <105342620+rniman@users.noreply.github.com>
2022-10-13 23:15:54 +0900

    실습13번 완성

--- 3_2_exercise/3_2_main.cpp
+++ 3_2_exercise/3_2_main.cpp
@@ -4,10 +4,9 @@
 
 GLvoid drawScene();
 GLvoid Reshape(int w, int h);
-GLvoid MouseClick(int button, int state, int x, int y);
-GLvoid MouseMotion(int x, int y);
-
+GLvoid TimeEvent(int value);
 GLvoid KeyEvent(unsigned char key, int x, int y);
+GLvoid SpecialKeyEvent(int key, int x, int y);
 void initBuffer();
 
 GLvoid convert_OpenglXY_WindowXY(int& x, int& y, const float& ox, const float& oy);
@@ -20,8 +19,11 @@ const GLint window_w = 600, window_h = 600;
 GLfloat rColor = 0.5f, gColor = 0.5f, bColor = 0.5f;
 GLboolean poly_state = true;
 GLboolean culling_on = false;
-GLboolean hexa_on[6] = { false, };
-GLboolean tetra_on[6] = { false, };
+GLboolean fill_on = true;
+GLboolean rotate_x = false;
+GLboolean dir_plus_x = true;
+GLboolean rotate_y = false;
+GLboolean dir_plus_y = true;
 
 GLuint vao[3];
 GLuint vbo_axes[2];
@@ -30,7 +32,7 @@ GLuint vbo_tetra[2];
 unsigned int modelLocation;
 
 axes_coordination axes;
-
+glm::mat4 obj_transformations = glm::mat4(1.0f);
 
 
 std::vector<GLfloat> hexa_vertex =
@@ -133,33 +135,47 @@ std::vector<GLfloat> hexa_color =
 	1.0f,0.0f,0.0f,
 	1.0f,0.0f,0.0f,
 };
-std::vector<GLfloat> tetra_vertex =
+std::vector<GLfloat> pyramid_vertex =
 {
-	//밑면
-	0.0f, 0.0f, 0.2f,
-	-0.1f, 0.0f, -0.1f,
-	0.1f, 0.0f, -0.1f,
+	//밑면1
+	0.3f, 0.0f, -0.3f,
+	0.3f, 0.0f, 0.3f,
+	-0.3f, 0.0f, -0.3f,
 
-	//뒷면
-	-0.1f, 0.0f, -0.1f,
-	0.1f, 0.0f, -0.1f,
-	0.0f, 0.4f, 0.0f,
+	//밑면2
+	-0.3f, 0.0f, 0.3f,
+	-0.3f, 0.0f, -0.3f,
+	0.3f, 0.0f, 0.3f,
 
+	//앞면
+	0.0f,0.4f,0.0f,
+	-0.3f, 0.0f, 0.3f,
+	0.3f, 0.0f, 0.3f,
+	
 	//왼쪽면
-	0.1f, 0.0f, -0.1f,
-	0.0f, 0.0f, 0.2f,
-	0.0f, 0.4f, 0.0f,
-
-	//오른쪽면
-	0.0f, 0.0f, 0.2f,
-	-0.1f, 0.0f, -0.1f,
-	0.0f, 0.4f, 0.0f,
+	0.0f,0.4f,0.0f,
+	0.3f, 0.0f, 0.3f,
+	0.3f, 0.0f, -0.3f,
+	
+	//뒷면
+	0.0f,0.4f,0.0f,
+	0.3f, 0.0f, -0.3f,
+	-0.3f, 0.0f, -0.3f,
+	
+	//오른쪽면	
+	0.0f,0.4f,0.0f,
+	-0.3f, 0.0f, -0.3f,
+	-0.3f, 0.0f, 0.3f,
 };
-std::vector<GLfloat> tetra_color =
+std::vector<GLfloat> pyramid_color =
 {
 	1.0f,1.0f,1.0f,
 	1.0f,1.0f,1.0f,
 	1.0f,1.0f,1.0f,
+	
+	1.0f,1.0f,1.0f,
+	1.0f,1.0f,1.0f,
+	1.0f,1.0f,1.0f,
 
 	1.0f,0.0f,0.0f,
 	1.0f,0.0f,0.0f,
@@ -171,14 +187,18 @@ std::vector<GLfloat> tetra_color =
 
 	0.0f,0.0f,1.0f,
 	0.0f,0.0f,1.0f,
-	0.0f,0.0f,1.0f
+	0.0f,0.0f,1.0f,
+
+	1.0f,1.0f,0.0f,
+	1.0f,1.0f,0.0f,
+	1.0f,1.0f,0.0f,
 };
 
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-	glutInitWindowPosition(100, 100);
+	glutInitWindowPosition(300, 200);
 	glutInitWindowSize(window_w, window_h);
 	glutCreateWindow("Example_2_5(10번)");
 
@@ -192,14 +212,18 @@ int main(int argc, char** argv)
 
 	glutDisplayFunc(drawScene);
 	glutReshapeFunc(Reshape);
-	glutMouseFunc(MouseClick);
-	glutMotionFunc(MouseMotion);
+	glutTimerFunc(100, TimeEvent, 0);
 	glutKeyboardFunc(KeyEvent);
-	glEnable(GL_DEPTH_TEST);
+	glutSpecialFunc(SpecialKeyEvent);
+
+	//glEnable(GL_DEPTH_TEST);
 
 	modelLocation = glGetUniformLocation(shaderID, "modelTransform");
-	axes.transformations = glm::rotate(axes.transformations, glm::radians(-30.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	axes.transformations = glm::rotate(axes.transformations, glm::radians(30.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 	axes.transformations = glm::rotate(axes.transformations, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+	obj_transformations = glm::rotate(obj_transformations, glm::radians(30.0f), glm::vec3(1.0, 0.0, 0.0));
+	obj_transformations = glm::rotate(obj_transformations, glm::radians(45.0f), glm::vec3(0.0, 1.0, 0.0));
 
 	glutMainLoop();
 }
@@ -207,6 +231,7 @@ int main(int argc, char** argv)
 
 GLvoid drawScene()
 {
+
 	glClearColor(rColor, gColor, bColor, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	//랜더링 파이프라인에 세이더 불러오기
@@ -215,19 +240,18 @@ GLvoid drawScene()
 	
 
 	modelLocation = glGetUniformLocation(shaderID, "modelTransform");
-
+	glDisable(GL_DEPTH_TEST);
 	//좌표축 그리기
 
 	glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(axes.transformations));
 	glBindVertexArray(vao[0]);
 	glDrawArrays(GL_LINES, 0, 6);
 
-	glm::mat4 Rz = glm::mat4(1.0f);
+	glEnable(GL_DEPTH_TEST);
+
 	//육면체 그리기
 
-	Rz = glm::rotate(Rz, glm::radians(-30.0f), glm::vec3(1.0, 0.0, 0.0));
-	Rz = glm::rotate(Rz, glm::radians(45.0f), glm::vec3(0.0, 1.0, 0.0));
-	glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(Rz));
+	glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(obj_transformations));
 	
 	if (poly_state)
 	{
@@ -237,13 +261,11 @@ GLvoid drawScene()
 	else
 	{
 		glBindVertexArray(vao[2]);
-		glDrawArrays(GL_TRIANGLES, 0, tetra_vertex.size() / 3);
+		glDrawArrays(GL_TRIANGLES, 0, pyramid_vertex.size() / 3);
 	}
 	
 
 
-
-	//glDrawArrays(GL_TRIANGLES, 0, hexa_vertex.size() / 3);
 	glutSwapBuffers();
 }
 
@@ -252,15 +274,26 @@ GLvoid Reshape(int w, int h)
 	glViewport(0, 0, w, h);
 }
 
-GLvoid MouseClick(int button, int state, int x, int y)
+GLvoid TimeEvent(int value)
 {
+	if (rotate_x)
+	{
+		if(dir_plus_x)
+			obj_transformations = glm::rotate(obj_transformations, glm::radians(10.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+		else
+			obj_transformations = glm::rotate(obj_transformations, glm::radians(-10.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	}
 
-}
-
-GLvoid MouseMotion(int x, int y)
-{
+	if (rotate_y)
+	{
+		if (dir_plus_y)
+			obj_transformations = glm::rotate(obj_transformations, glm::radians(10.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		else
+			obj_transformations = glm::rotate(obj_transformations, glm::radians(-10.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	}
 
 	glutPostRedisplay();
+	glutTimerFunc(100, TimeEvent, 0);
 }
 
 GLvoid KeyEvent(unsigned char key, int x, int y)
@@ -283,9 +316,68 @@ GLvoid KeyEvent(unsigned char key, int x, int y)
 			glDisable(GL_CULL_FACE);
 		}
 	}
+	else if (key == 'x')
+	{
+		rotate_x = true;
+		dir_plus_x = true;
+	}
+	else if (key == 'X')
+	{
+		rotate_x = true;
+		dir_plus_x = false;
+	}
+	else if (key == 'y')
+	{
+		rotate_y = true;
+		dir_plus_y = true;
+	}
+	else if (key == 'Y')
+	{
+		rotate_y = true;
+		dir_plus_y = false;
+	}
+	else if (key == 'w')
+	{
+		fill_on = fill_on ? false : true;
+		if (fill_on)
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		else
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	}
+	else if (key == 's')
+	{
+		rotate_x = false;
+		rotate_y = false;
+		obj_transformations = glm::mat4(1.0f);
+		obj_transformations = glm::rotate(obj_transformations, glm::radians(30.0f), glm::vec3(1.0, 0.0, 0.0));
+		obj_transformations = glm::rotate(obj_transformations, glm::radians(45.0f), glm::vec3(0.0, 1.0, 0.0));
+	}
 
-	glutPostRedisplay();
+
+
+	//glutPostRedisplay();
 }
+
+GLvoid SpecialKeyEvent(int key, int x, int y)
+{
+	if (key == GLUT_KEY_RIGHT)
+	{
+		obj_transformations = glm::translate(obj_transformations, glm::vec3(0.01f, 0.0f, 0.0f));
+	}
+	else if (key == GLUT_KEY_LEFT)
+	{
+		obj_transformations = glm::translate(obj_transformations, glm::vec3(-0.01f, 0.0f, 0.0f));
+	}
+	else if (key == GLUT_KEY_UP)
+	{
+		obj_transformations = glm::translate(obj_transformations, glm::vec3(0.0f, 0.01f, 0.0f));
+	}
+	else if (key == GLUT_KEY_DOWN)
+	{
+		obj_transformations = glm::translate(obj_transformations, glm::vec3(0.0f, -0.01f, 0.0f));
+	}
+}
+
 
 void initBuffer()
 {
@@ -320,61 +412,12 @@ void initBuffer()
 	glBindVertexArray(vao[2]);
 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo_tetra[1]);
-	glBufferData(GL_ARRAY_BUFFER, tetra_color.size() * sizeof(GLfloat), tetra_color.data(), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, pyramid_color.size() * sizeof(GLfloat), pyramid_color.data(), GL_STATIC_DRAW);
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(1);
 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo_tetra[0]);
-	glBufferData(GL_ARRAY_BUFFER, tetra_vertex.size() * sizeof(GLfloat), tetra_vertex.data(), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, pyramid_vertex.size() * sizeof(GLfloat), pyramid_vertex.data(), GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(0);
-}
-
-GLvoid convert_OpenglXY_WindowXY(int& x, int& y, const float& ox, const float& oy)
-{
-	x = static_cast<int>((window_w / 2) + ox * (window_w / 2));
-	y = static_cast<int>((window_h / 2) - oy * (window_h / 2));
-}
-
-GLvoid convert_WindowXY_OpenglXY(const int& x, const int& y, float& ox, float& oy)
-{
-	ox = (float)((x - (float)window_w / 2.0) * (float)(1.0 / (float)(window_w / 2.0)));
-	oy = -(float)((y - (float)window_h / 2.0) * (float)(1.0 / (float)(window_h / 2.0)));
-}
-
-GLint check_mouse_point(const GLfloat& ox, const GLfloat& oy, const std::vector<GLfloat>& ver)
-{
-	for (int i = 0; i < 4; ++i)
-	{
-		if (ox > ver[i * 3] - 0.0125f && ox < ver[i * 3] + 0.0125f && oy > ver[i * 3 + 1] - 0.0125f && oy < ver[i * 3 + 1] + 0.0125f)
-		{
-			return i + 1;
-		}
-	}
-
-	if (isInside(ox, oy, ver))
-		return 0;
-
-	return -1;
-}
-
-GLboolean isInside(const GLfloat& ox, const GLfloat& oy, const std::vector<GLfloat>& ver)
-{
-	//crosses는 점q와 오른쪽 반직선과 다각형과의 교점의 개수 
-	int crosses = 0;
-	for (int i = 0; i < 4; i++)
-	{
-		int j = (i + 1) % 4;
-		//점 B가 선분 (p[i], p[j])의 y좌표 사이에 있음    
-		if ((ver[i * 3 + 1] > oy) != (ver[j * 3 + 1] > oy))
-		{
-			//atX는 점 B를 지나는 수평선과 선분 (p[i], p[j])의 교점     
-			double atX = (ver[j * 3] - ver[i * 3]) * (oy - ver[i * 3 + 1]) / (ver[j * 3 + 1] - ver[i * 3 + 1]) + ver[i * 3];
-
-			//atX가 오른쪽 반직선과의 교점이 맞으면 교점의 개수를 증가시킨다.     
-			if (ox < atX)
-				crosses++;
-		}
-	}
-	return crosses % 2 > 0;
 }
